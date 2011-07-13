3683b846a9b9b9d3c06811f03797519ac5da295d
Lorenzo Urbini <superluglor@gmail.com>
2011-07-13 12:10:16 +0100

    UI adjustments pt.2

--- lunchexpress/app/controllers/home_controller.rb
+++ lunchexpress/app/controllers/home_controller.rb
@@ -1,4 +1,11 @@
+require 'fusion_tables'
+
 class HomeController < ApplicationController
+  
+  # Connect to service    
+  @ft = GData::Client::FusionTables.new      
+  @ft.clientlogin('superluglor', 'lugano83')
+  
   def index
   end
 
