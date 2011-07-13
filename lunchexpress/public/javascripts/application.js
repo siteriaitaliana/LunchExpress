913e2bc5ab904e236e53c69b564466632801d5d0
Lorenzo Urbini <lorenzo.urbini@edgeint.net>
2011-07-13 21:03:36 +0100

    bring back

--- lunchexpress/public/javascripts/application.js
+++ lunchexpress/public/javascripts/application.js
@@ -5,6 +5,12 @@
   var centermap;
   var tableid = 1128316;
   
+  function runScript(e) {
+    if (e.keyCode == 13) 
+        codeAddress();
+  }
+
+  
   function initialize() {
     geocoder = new google.maps.Geocoder();
     var latlng = new google.maps.LatLng(51.5010, -0.12617);
