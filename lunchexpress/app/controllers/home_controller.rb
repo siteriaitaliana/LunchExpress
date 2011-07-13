3a61157985a2ccf0ea8b70c97e1ed8be5a0ea7d6
Lorenzo Urbini <superluglor@gmail.com>
2011-07-13 16:45:20 +0100

    understood how to generate the order link in the gmapballon (infoWindowHtml)

--- lunchexpress/app/controllers/home_controller.rb
+++ lunchexpress/app/controllers/home_controller.rb
@@ -1,10 +1,4 @@
-require 'fusion_tables'
-
 class HomeController < ApplicationController
-  
-  # Connect to service    
-  @ft = GData::Client::FusionTables.new      
-  @ft.clientlogin('superluglor', 'lugano83')
   
   def index
   end
--- lunchexpress/public/javascripts/application.js
+++ lunchexpress/public/javascripts/application.js
@@ -4,11 +4,12 @@
   var map;
   var centermap;
   var tableid = 1128316;
+  
   function initialize() {
     geocoder = new google.maps.Geocoder();
     var latlng = new google.maps.LatLng(51.5010, -0.12617);
     var myOptions = {
-      zoom: 14,
+      zoom: 15,
       center: latlng,
       disableDefaultUI: true,
       navigationControl: true,
@@ -26,7 +27,7 @@
 
   function codeAddress() {
   	initialize();
-    var address = document.getElementById("address").value;
+    address = document.getElementById("address").value;
     geocoder.geocode( { 'address': address}, function(results, status) {
       if (status == google.maps.GeocoderStatus.OK) {
         map.setCenter(results[0].geometry.location);
@@ -44,25 +45,23 @@
   }
   
   function retrievePlaces() {
-  	//alert(centermap.toString());
   	layer = new google.maps.FusionTablesLayer(tableid);
   	layer.setMap(map);
-  	layer.setQuery("SELECT * FROM "+tableid+" WHERE ST_INTERSECTS(geo, CIRCLE(LATLNG"+centermap+", 1500))");
-  	
+  	layer.setQuery("SELECT * FROM "+tableid+" WHERE ST_INTERSECTS(geo, CIRCLE(LATLNG"+centermap+", 1000))");
   
     circle = new google.maps.Circle({
 	    center: centermap,
-	    radius: 1500,
+	    radius: 1000,
 	    map: map,
 	    fillOpacity: 0.2,
 	    strokeOpacity: 0.5,
 	    strokeWeight: 1
   	});
-
-  	
-	//add a click listener to the layer
-  	//google.maps.event.addListener(layer, 'click', function(e) {
-    //update the content of the InfoWindow
-    //e.infoWindowHtml = e.row['Store Name'].value + "<br />";
-  
+  	/*add a click listener to the layer
+  	google.maps.event.addListener(layer, 'click', function(e) {
+    update the content of the InfoWindow
+   	e.infoWindowHtml = e.row['iframe'].value;
+ 	}); */
+	
   }
+
